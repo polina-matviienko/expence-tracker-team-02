105a373fee0bee349f76664d09b07656e00d8651
Polina Matviienko <polinamatviienko030@gmail.com>
2026-04-08 18:14:51 +0300

    add auth-aware navigation in Logo

--- components/Layout/Logo/Logo.tsx
+++ components/Layout/Logo/Logo.tsx
@@ -1,15 +1,21 @@
+'use client';
+
 import Link from 'next/link';
+import { useAuthStore } from '@/lib/store/authStore';
 import css from './Logo.module.css';
 
-function Logo() {
+const Logo = () => {
+  const isAuthenticated = useAuthStore(state => state.isAuthenticated);
+  const logoHref = isAuthenticated ? '/transactions/expenses' : '/';
+
   return (
-    <Link href="/" className={css.logo} aria-label="Home">
+    <Link href={logoHref} className={css.logo} aria-label="Home">
       <svg className={css.icon} width="27" height="16">
         <use href="/icons.svg#logo-icon" />
       </svg>
-      <span className={css.text}>ExpenseTracker</span>
+      <span className={css.text}>EXPENSETRACKER</span>
     </Link>
   );
-}
+};
 
 export default Logo;
